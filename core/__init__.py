67a675dd78c89cb92cb5743121aa362d46171593
plseal <ttml0122@gmail.com>
2024-05-27 11:30:27 +0900

    Update __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -1,2 +1,2 @@
-from .evaluation import run_eval, fix_indents, filter_code, split_batch
+from .evaluation import run_eval,run_eval_one, fix_indents, filter_code, split_batch
 from .prompts import instruct_prompt, standard_prompt, replit_glaive_prompt
